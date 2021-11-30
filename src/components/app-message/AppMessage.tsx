80db7d2a048d4a8f46e1b7d6b3473b34471de209
alexander <aleksander9999a@gmail.com>
2021-11-30 23:13:04 +0200

    Added message layout

--- src/components/app-message/AppMessage.tsx
+++ src/components/app-message/AppMessage.tsx
@@ -1,12 +1,39 @@
+// React
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, View, Text } from 'react-native'
+
+// Interfaces
+import { IAppMessageProps } from './interfaces'
 
 const styles = StyleSheet.create({
-  container: {}
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 8
+  },
+  justifyEnd: {
+    justifyContent: 'flex-end'
+  },
+  justifyStart: {
+    justifyContent: 'flex-start'
+  },
+  content: {
+    borderRadius: 8,
+    padding: 6,
+    borderWidth: 1,
+    borderColor: '#000',
+    maxWidth: '70%',
+    minWidth: '30%'
+  }
 })
 
-export default function AppMessage () {
+export default function AppMessage ({ message, isLeft }: IAppMessageProps) {
   return (
-    <View style={styles.container}></View>
+    <View style={[styles.container, isLeft ? styles.justifyStart : styles.justifyEnd]}>
+      <View style={styles.content}>
+        <Text>{message.text}</Text>
+      </View>
+    </View>
   )
 }
