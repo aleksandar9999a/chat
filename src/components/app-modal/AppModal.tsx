4b32b5092d4d3ee86a57bd7c5af37346af357c18
alexander <aleksander9999a@gmail.com>
2021-11-30 21:52:44 +0200

    Added modal layout

--- src/components/app-modal/AppModal.tsx
+++ src/components/app-modal/AppModal.tsx
@@ -1,12 +1,38 @@
+// React
 import React from 'react'
 import { StyleSheet, View } from 'react-native'
 
+// Interfaces
+import { IAppModalProps } from './interfaces'
+
+
 const styles = StyleSheet.create({
-  container: {}
+  container: {
+    position: 'absolute',
+    top: 0,
+    bottom: '-92vh',
+    left: 0,
+    right: 0,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, .7)'
+  },
+  content: {
+    minWidth: '80vw'
+  }
 })
 
-export default function AppModal () {
+export default function AppModal ({ isOpen, children }: IAppModalProps) {
+  if (!isOpen) {
+    return null
+  }
+
   return (
-    <View style={styles.container}></View>
+    <View style={styles.container}>
+      <View style={styles.content}>
+        {children}
+      </View>
+    </View>
   )
 }
