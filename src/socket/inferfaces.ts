ba9451f540a57b6d16548d64c407c63f77b0f9e2
alexander <aleksander9999a@gmail.com>
2021-12-01 23:30:29 +0200

    Added socket interface

--- src/socket/inferfaces.ts
+++ src/socket/inferfaces.ts
@@ -1,3 +1,5 @@
+import { IReceivedMessage, ISendMessage, ITyping } from '../interfaces'
+
 export interface IConfig {
   socket: string,
   owner: string,
@@ -8,4 +10,12 @@ export interface ISub {
   id: string,
   fn: (data: any) => any,
   unsubscribe: () => void
+}
+
+export interface ISocket {
+  onMessage: (fn: (msg: IReceivedMessage) => any) => ISub,
+  onTyping: (fn: (msg: ITyping) => any) => ISub,
+  startTyping: () => void,
+  stopTyping: () => void,
+  sendMessage: (data: ISendMessage) => void
 }
