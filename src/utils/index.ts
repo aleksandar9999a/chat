f4c06d17ac66dbaed5055a24ddbab224ae57888f
alexander <aleksander9999a@gmail.com>
2021-12-06 21:22:48 +0200

    Remove debug

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,6 +2,9 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { v4 as uuidv4 } from 'uuid'
 
+// Config
+import { MESSAGES, USER } from '../configs/storage'
+
 // Interfaces
 import { IReceivedMessage } from '../interfaces'
 import { IStorageData } from './interfaces'
@@ -12,7 +15,7 @@ function getUser (user: string|null): string {
     const newUser = uuidv4()
 
     // We automatically save new user as a side effect
-    AsyncStorage.setItem('user', newUser)
+    AsyncStorage.setItem(USER, newUser)
 
     return newUser
   }
@@ -29,15 +32,14 @@ function getMessages (messages: string|null): IReceivedMessage[] {
 }
 
 export function setMessages (messages: IReceivedMessage[]) {
-  return AsyncStorage.setItem('messages', JSON.stringify(messages))
+  return AsyncStorage.setItem(MESSAGES, JSON.stringify(messages))
 }
 
 export function loadStorageData (): Promise<IStorageData> {
-  const keys = ['user', 'messages']
+  const keys = [USER, MESSAGES]
 
   return Promise.all(keys.map(x => AsyncStorage.getItem(x)))
     .then(([user, messages]) => {
-      console.debug({ user, messages })
       return {
         user: getUser(user),
         messages: getMessages(messages)
